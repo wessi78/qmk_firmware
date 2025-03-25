c42209d6452af3165f5f19d0fc94dcb6453c37e2
wessi78 <159825660+wessi78@users.noreply.github.com>
2025-03-25 11:11:39 +0100

    Replace CapsLock with empty key

--- keyboards/keychron/k7_pro/iso/rgb/keymaps/wessi78/keymap.c
+++ keyboards/keychron/k7_pro/iso/rgb/keymaps/wessi78/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_69_iso(
         KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_DEL,
         KC_TAB,   KC_Q,     LT(0,KC_W),     LT(0,KC_E),     LT(0,KC_R),     KC_T,     LT(0,KC_Y),     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,                      KC_HOME,
-        KC_CAPS,  LT(0,KC_A),     KC_S,     KC_D,     LT(0,KC_F),     KC_G,     LT(0,KC_H),     KC_J,     KC_K,     LT(0,KC_L),     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,             KC_PGUP,
+        _______,  LT(0,KC_A),     KC_S,     KC_D,     LT(0,KC_F),     KC_G,     LT(0,KC_H),     KC_J,     KC_K,     LT(0,KC_L),     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,             KC_PGUP,
         KC_LSFT,  KC_NUBS,  KC_Z,     LT(0,KC_X),     LT(0,KC_C),     LT(0,KC_V),     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_PGDN,
         KC_LCTL,  KC_LOPT, KC_LCMD,                                 KC_SPC,                                 KC_ROPT, MO(MAC_FN1),MO(FN2), KC_LEFT,  KC_DOWN,  KC_RGHT),
 
